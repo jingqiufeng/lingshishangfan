75424686911de4cb0dfc8dcd8f2a7a3e9870d29e
youguohua <hiyouguohua@qq.com>
2019-12-30 21:57:06 +0800

    11-19看完

--- pages/product/product.js
+++ pages/product/product.js
@@ -8,7 +8,10 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+      id:null,
+      countsArray:[1,2,3,4,5,6,7,8,9,10],
+      productCount:1,
+      currentTabsIndex:0
   },
 
   /**
@@ -24,6 +27,21 @@ Page({
       this.setData({
         product:data
       });
+    });
+  },
+
+  bindPickerChange:function(event){
+    var index = event.detail.value;
+    var selectedCount = this.data.countsArray[index];
+    this.setData({
+      productCount:selectedCount
+    });
+  },
+
+  onTabsItemTap:function(event){
+    var index = product.getDataSet(event,'index');
+    this.setData({
+      currentTabsIndex:index
     });
   }
 
